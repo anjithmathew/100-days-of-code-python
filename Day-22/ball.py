c0c5d779bced91812f31996c1c864ee3e73ae825
Anjith Mathew <anjithmathew777@gmail.com>
2022-11-02 22:04:08 +0530

    unfinished

--- Day-22/ball.py
+++ Day-22/ball.py
@@ -1,8 +1,22 @@
-from turtle import Turtle,Screen
+from turtle import Turtle
 
-screen = Screen()
-screen.title("amal Scaria ")
-screen.bgcolor("black")
-turtle= Turtle("square")
-turtle.color("white")
-screen.exitonclick()
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move =10
+        self.ymove =10
+    def move(self):
+        new_x = self.xcor()+self.x_move
+        new_y = self.ycor()+self.y_move
+        self.move(new_x, new_y)
+    
+    def bounceY(self):
+        new_y = self.ymove *=-1
+    
+    def bounceX(self):
+        new_X = self.Xmove *=-1
+
--- Day-22/main.py
+++ Day-22/main.py
@@ -1,4 +1,7 @@
 from turtle import Screen, Turtle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,10 +9,22 @@ screen.setup(800, 600)
 screen.title("pong")
 screen.tracer(0)
 
+
+
+
+
 game_is_on = True
 
 while game_is_on:
     screen.update()
-
+    ball.move()
+    if ball().ycor >300 or  if ball().ycor < 300:
+        ball.bounce()
     
+
+    if ball.distance(r_paddle) <50 and ball.xcor() >340 or ball.distance(l_paddel )<:
+
+    #detect right paddle miiises
+
+        
 screen.exitonclick()
