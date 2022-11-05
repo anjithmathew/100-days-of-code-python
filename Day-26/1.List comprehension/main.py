8986bbec7d1dde3c1d70a9eccf2d5fc5464ce0e0
Anjith Mathew <anjithmathew777@gmail.com>
2022-11-05 23:55:54 +0530

    27

--- Day-26/1.List comprehension/main.py
+++ Day-26/1.List comprehension/main.py
@@ -3,3 +3,13 @@ n = [1,2,3]
 new_list = [i+1 for i in n ]
 
 print(new_list)
+
+name = "Anjith"
+
+name_new =  [i+"1" for i in name]
+
+print(name_new)
+
+name_1 = [1*"aaa" for i in range(1,4)]
+
+print(name_1)
