6d27e706202c30ee51a117a139805ed3b43485ce
Anjith Mathew <anjithmathew777@gmail.com>
2022-11-12 13:28:30 +0530

    kanye

--- Day-33/2.kanye west/main.py
+++ Day-33/2.kanye west/main.py
@@ -1,9 +1,10 @@
 from tkinter import *
-
+import requests
 
 def get_quote():
-    pass
-    #Write your code here.
+    data_get = requests.get('https://api.kanye.rest/')
+    quotes = data_get.json()
+    return quotes['quote']
 
 
 
@@ -14,13 +15,12 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="/home/anjithmathew/Documents/1.Python/100-days-of-code-python/Day-33/2.kanye west/background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text=get_quote(), width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="/home/anjithmathew/Documents/1.Python/100-days-of-code-python/Day-33/2.kanye west/kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=gquote)
 kanye_button.grid(row=1, column=0)
-
 
 
 window.mainloop()
