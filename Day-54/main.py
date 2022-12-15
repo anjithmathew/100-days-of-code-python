732db1d7951eff7597b50e0cbe8af7f77b267c79
anjithmathew <anjithmathew777@gmail.com>
2022-12-15 18:31:51 +0530

    flask

--- Day-54/main.py
+++ Day-54/main.py
@@ -2,6 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+def bold(funcion):
+    """ Wrapper function to bold"""
+    def wrapper():
+        function()
+        return wrapper()
 @app.route("/")
 def world():
-    return "<p>Hello, World!</p>"
+    return '<h1 style:"text-align: center">Hello, World!</h1>'\
+        '<p> Hello Guyz</p>'\
+        '<img src =">'
+
+
+@app.route("/bye")
+def bye():
+    return "bye"
+
+@app.route("/<name>")
+def greet(name):
+    return f"buddy    {na}"
+
+if __name__ =="__main__" :
+    app.run(debug=True)
